4d8641862dca6cf4d58857219f592d5b6802f3e5
Diego dev <diego.oliveiraamaro@gmail.com>
2026-02-25 09:06:32 -0300

    teste6

--- Games.Api/Games.Api/Program.cs
+++ Games.Api/Games.Api/Program.cs
@@ -68,7 +68,7 @@ app.UseSwagger();
 
 app.UseSwaggerUI(c =>
 {
-    //c.SwaggerEndpoint("/swagger/v1/swagger.json", "Games API v1");
+    // c.SwaggerEndpoint("/swagger/v1/swagger.json", "Games API v1");
     c.SwaggerEndpoint("/games/swagger/v1/swagger.json", "Games API v1");
     c.RoutePrefix = "swagger";
 });
